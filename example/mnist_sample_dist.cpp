628c8c93899b6ab4817fcacd2f7a3ecd8fec6c97
Gyuuto <gyuuto@gmail.com>
2016-05-23 22:02:34 +0900

    Update README.md and added distributed mnist sample.

---
+++ example/mnist_sample_dist.cpp
@@ -0,0 +1,219 @@
+#include <iostream>
+#include <fstream>
+#include <functional>
+#include <memory>
+#include <cmath>
+#include <cstring>
+
+#include "../include/Neuralnet.hpp"
+#include "../include/Layer.hpp"
+#include "../include/FullyConnected.hpp"
+#include "../include/Convolutional.hpp"
+#include "../include/Pooling.hpp"
+
+using namespace std;
+
+// pixel normalize function
+void normalize ( vector<vector<vector<double>>>& image, vector<vector<double>>& ave )
+{
+    for( int i = 0; i < image[0].size(); ++i ){
+        ave.emplace_back(image[0][0].size(), 0.0);
+
+        for( int j = 0; j < image.size(); ++j ){
+            for( int k = 0; k < image[0][i].size(); ++k ) ave[i][k] += image[j][i][k];
+        }
+        for( int j = 0; j < image[0][i].size(); ++j ) ave[i][j] /= image.size();
+
+        for( int j = 0; j < image.size(); ++j )
+			for( int k = 0; k < image[0][i].size(); ++k )
+				image[j][i][k] -= ave[i][k];
+    }
+}
+
+int main( int argc, char* argv[] )
+{
+	// define mini-batch size.
+	const int BATCH_SIZE = 50;
+
+	// parallelism for model parallel.
+	int INNER = 1;
+	for( int i = 1; i < argc; ++i ){
+		if( strcmp(argv[i], "--inner") == 0 ){
+			sscanf(argv[i+1], "%d", &INNER);
+			++i;
+		}
+	}
+
+	MPI_Init(&argc, &argv);
+
+	MPI_Comm outer_world, inner_world;
+	int world_rank, world_nprocs;       // rank and number of processes in all processes.
+	int inner_rank, inner_nprocs;       // rank and number of processes in model parallel.
+	int outer_rank, outer_nprocs;       // rank and number of processes in data parallel.
+
+	MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &world_nprocs);
+
+	// determine model parallel's rank of each processes.
+	MPI_Comm_split(MPI_COMM_WORLD, world_rank / INNER, world_rank, &inner_world);
+	MPI_Comm_rank(inner_world, &inner_rank);
+	MPI_Comm_size(inner_world, &inner_nprocs);
+
+	// determine data parallel's rank of each processes.
+	MPI_Comm_split(MPI_COMM_WORLD, world_rank % INNER, world_rank, &outer_world);
+	MPI_Comm_rank(outer_world, &outer_rank);
+	MPI_Comm_size(outer_world, &outer_nprocs);
+
+	// construct neuralnetwork with CrossEntropy.
+    Neuralnet net(shared_ptr<LossFunction>(new CrossEntropy), outer_world, inner_world);
+	vector<shared_ptr<Layer>> layers;
+
+	// define layers.
+	layers.emplace_back(new Convolutional(1, 28*28, 28,
+										  10, 28*28, 28,
+										  5, 5, 1, shared_ptr<Function>(new ReLU)));
+	layers.emplace_back(new Pooling(10, 28*28, 28,
+									10, 14*14, 14,
+									2, 2, 2, shared_ptr<Function>(new Identity)));
+	layers.emplace_back(new Convolutional(10, 14*14, 14,
+										  20, 14*14, 14,
+										  5, 5, 1, shared_ptr<Function>(new ReLU)));
+	layers.emplace_back(new Pooling(20, 14*14, 14,
+									20, 7*7, 7,
+									2, 2, 2, shared_ptr<Function>(new Identity)));
+	layers.emplace_back(new FullyConnected(20, 7*7, 1, 512, shared_ptr<Function>(new ReLU)));
+	layers.emplace_back(new FullyConnected(1, 512, 1, 10, shared_ptr<Function>(new Softmax)));
+
+	// this neuralnet has 7 layers, input, Conv1, MaxPool, Conv2, MaxPool, Fc1 and Fc2;
+	for( int i = 0; i < layers.size(); ++i ){
+		net.add_layer(layers[i]);
+	}
+	
+	// read a test data of MNIST(http://yann.lecun.com/exdb/mnist/).
+	vector<int> train_lab;
+	vector<vector<vector<double>>> train_x;
+	const int N = 10000 / outer_nprocs;
+	ifstream train_image("train-images-idx3-ubyte", ios_base::binary);
+	ifstream train_label("train-labels-idx1-ubyte", ios_base::binary);
+	train_image.seekg(4*4 + 28*28*outer_rank * N, ios_base::beg);
+	train_label.seekg(4*2 + outer_rank * N, ios_base::beg);
+	for( int i = 0; i < N; ++i ){
+		unsigned char tmp_lab;
+		train_label.read((char*)&tmp_lab, sizeof(unsigned char));
+		train_lab.push_back(tmp_lab);
+		
+		vector<vector<double>> tmp(1, vector<double>(28*28));
+		for( int j = 0; j < 28*28; ++j ){
+			unsigned char c;
+			train_image.read((char*)&c, sizeof(unsigned char));
+			tmp[0][j] = (c/255.0);
+		}
+		
+		train_x.push_back(tmp);
+	}
+	
+	// set supervised data.
+	vector<vector<vector<double>>> d(N, vector<vector<double>>(1, vector<double>(10, 0.0)));
+	for( int i = 0; i < N; ++i ) d[i][0][train_lab[i]] = 1.0;
+
+	vector<vector<double>> ave;
+	// normalize train image.
+	normalize(train_x, ave);
+
+	// read a train data of MNIST.
+	vector<int> test_lab;
+	vector<vector<vector<double>>> test_x;
+	const int M = 5000;
+	ifstream test_image("t10k-images-idx3-ubyte", ios_base::binary);
+	ifstream test_label("t10k-labels-idx1-ubyte", ios_base::binary);
+	test_image.seekg(4*4, ios_base::beg);
+	test_label.seekg(4*2, ios_base::beg);
+	for( int i = 0; i < M; ++i ){
+		unsigned char tmp_lab;
+		test_label.read((char*)&tmp_lab, sizeof(unsigned char));
+		test_lab.push_back(tmp_lab);
+		
+		vector<vector<double>> tmp(1, vector<double>(28*28));
+		for( int j = 0; j < 28*28; ++j ){
+			unsigned char c;
+			test_image.read((char*)&c, sizeof(unsigned char));
+			tmp[0][j] = (c/255.0 - ave[0][j]);
+		}
+		
+		test_x.push_back(tmp);
+	}
+
+	// train data into Matrix class for checking error.
+	vector<Matrix<double>> X(1, Matrix<double>(28*28, M)), D(1, Matrix<double>(10, M));
+	for( int i = 0; i < M; ++i ){
+		for( int j = 0; j < 28*28; ++j ){
+			X[0](j, i) = test_x[i][0][j];
+		}
+		D[0](test_lab[i], i) = 1.0;
+	}
+
+	// checking error function.
+	double prev_time, total_time;
+	auto check_error = [&](const Neuralnet& nn, const int iter, const std::vector<Matrix<double>>& x, const std::vector<Matrix<double>>& d ) -> void {
+		if( iter%((N/BATCH_SIZE)) != 0 ) return;
+
+		double tmp_time = MPI_Wtime();
+
+		// calculating answer rate among all train data.
+		int train_ans_num = 0;
+		auto y = nn.apply(train_x);
+		for( int i = 0; i < y.size(); ++i ){
+			int idx, lab;
+			double max_num = 0.0;
+			for( int j = 0; j < 10; ++j ){
+				if( max_num < y[i][0][j] ){
+					max_num = y[i][0][j];
+					idx = j;
+				}
+			}
+			if( idx == train_lab[i] ) ++train_ans_num;
+		}
+
+		// calculating answer rate among all test data.
+		int test_ans_num = 0;
+		auto Y = nn.apply(X);
+		for( int i = 0; i < Y[0].n; ++i ){
+			int idx, lab;
+			double max_num = 0.0;
+			for( int j = 0; j < 10; ++j ){
+				if( max_num < Y[0](j,i) ){
+					max_num = Y[0](j,i);
+					idx = j;
+				}
+				if( D[0](j, i) == 1.0 ) lab = j;
+			}
+			if( idx == lab ) ++test_ans_num;
+		}
+
+		// output answer rate each test data batch.
+		if( inner_rank == 0 ){
+			for( int i = 0; i < outer_nprocs; ++i ){
+				if( i == outer_rank ){
+					printf("outer rank : %d\n", outer_rank);
+					printf("  Elapsed time : %.3f, Total time : %.3f\n", MPI_Wtime() - prev_time, MPI_Wtime() - total_time);
+					printf("  Train answer rate : %.2f%%\n", (double)train_ans_num/y.size()*100.0);
+					printf("  Test answer rate  : %.2f%%\n", (double)test_ans_num/X[0].n*100.0);
+				}
+				MPI_Barrier(outer_world);
+			}
+			prev_time = MPI_Wtime();
+		}
+		if( world_rank == 0 ) puts("");
+	};
+
+	// set a hyper parameter.
+	net.set_EPS(1.0E-3);
+	net.set_LAMBDA(0.0);
+	net.set_BATCHSIZE(BATCH_SIZE);
+	net.set_UPDATEITER(N/BATCH_SIZE*5);
+	// learning the neuralnet in 20 EPOCH and output error defined above in each epoch.
+	prev_time = total_time = MPI_Wtime();
+	net.learning(train_x, d, N/BATCH_SIZE*20, check_error);
+
+	MPI_Finalize();
+}
