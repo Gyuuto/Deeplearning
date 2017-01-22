b4d5e63b84daa843fec7668e7171ebeefc4ac4da
Yuto INOUE <yuto@mma.cs.tsukuba.ac.jp>
2017-01-23 00:12:58 +0900

    Fixed bugs in running not MPI version.

--- include/BatchNormalize.hpp
+++ include/BatchNormalize.hpp
@@ -101,23 +101,24 @@ std::vector<std::vector<BatchNormalize::Mat>> BatchNormalize::calc_gradient ( co
 	my_offset = 0;
 	my_size = prev_num_unit;
 #endif
-	std::vector<std::vector<Mat>> nabla(1, std::vector<Mat>(num_map, Mat::zeros(1, 2)));
+	std::vector<std::vector<Mat>> nabla(1, std::vector<Mat>(num_map, Mat(1, 2)));
 	auto end = std::chrono::system_clock::now();
 	t_grad_init += std::chrono::duration_cast<std::chrono::nanoseconds>(end - beg).count()/1e9;
 
 	for( int i = 0; i < num_map; ++i ){
-		double tmp_nabla1 = 0.0, tmp_nabla2 = 0.0;;
+		auto U_appl = (*prev_func)(U[i], false);
+		double tmp_nabla1 = 0.0, tmp_nabla2 = 0.0;
 		auto beg = std::chrono::system_clock::now();
 #pragma omp parallel
 		{
-#pragma omp for schedule(auto) reduction(+:tmp_nabla1)
+#pragma omp for schedule(auto) nowait reduction(+:tmp_nabla1)
 			for( int j = 0; j < my_size; ++j ){
 				for( int k = 0; k < U[i].n; ++k ){
-					tmp_nabla1 += delta[i](my_offset+j,k)*(U[i](my_offset+j,k) - mean(i,j))/std::sqrt(var(i,j) + EPS);
+					tmp_nabla1 += delta[i](my_offset+j,k)*(U_appl(my_offset+j,k) - mean(i,j))/std::sqrt(var(i,j) + EPS);
 				}
 			}
 
-#pragma omp for schedule(auto) reduction(+:tmp_nabla2)
+#pragma omp for schedule(auto) nowait reduction(+:tmp_nabla2)
 			for( int j = 0; j < my_size; ++j ){
 				for( int k = 0; k < U[i].n; ++k ){
 					tmp_nabla2 += delta[i](my_offset+j,k);
@@ -165,7 +166,9 @@ std::vector<BatchNormalize::Mat> BatchNormalize::calc_delta ( const std::vector<
 #endif
 
 	std::vector<Mat> nx_delta(prev_num_map, Mat(prev_num_unit, delta[0].n));
+#ifdef USE_MPI
 	std::vector<Mat> tmp_nx_delta(prev_num_map, Mat(my_size, delta[0].n));
+#endif
 	auto end = std::chrono::system_clock::now();
 	t_delta_init += std::chrono::duration_cast<std::chrono::nanoseconds>(end - beg).count()/1e9;
 
@@ -184,9 +187,15 @@ std::vector<BatchNormalize::Mat> BatchNormalize::calc_delta ( const std::vector<
 				}
 				tmp1 /= U[i].n; tmp2 /= U[i].n;
 
+#ifdef USE_MPI
 				tmp_nx_delta[i](j,k) = W[0][i](0,0)/sqrt(var(i,j) + EPS)*delta[i](my_offset+j,k)*U_diff(my_offset+j,k)
 					- W[0][i](0,0)/sqrt(var(i,j) + EPS)*U_diff(my_offset+j,k)*tmp1
 					- W[0][i](0,0)/pow(var(i,j) + EPS, 1.5)*U_diff(my_offset+j,k)*(U_appl(my_offset+j,k) - mean(i,j))*tmp2;
+#else
+				nx_delta[i](j,k) = W[0][i](0,0)/sqrt(var(i,j) + EPS)*delta[i](my_offset+j,k)*U_diff(my_offset+j,k)
+					- W[0][i](0,0)/sqrt(var(i,j) + EPS)*U_diff(my_offset+j,k)*tmp1
+					- W[0][i](0,0)/pow(var(i,j) + EPS, 1.5)*U_diff(my_offset+j,k)*(U_appl(my_offset+j,k) - mean(i,j))*tmp2;
+#endif
 			}
 		auto end = std::chrono::system_clock::now();
 		t_delta_gemm += std::chrono::duration_cast<std::chrono::nanoseconds>(end - beg).count()/1e9;
@@ -235,35 +244,50 @@ std::vector<BatchNormalize::Mat> BatchNormalize::apply ( const std::vector<Mat>&
 	mean = var = Mat::zeros(num_map, my_size);
 
 	std::vector<Mat> ret(num_map, Mat(num_unit, U[0].n));
+#ifdef USE_MPI
 	std::vector<Mat> tmp_ret(num_map, Mat(my_size, U[0].n));
+#endif
 	auto end = std::chrono::system_clock::now();
 	t_apply_init += std::chrono::duration_cast<std::chrono::nanoseconds>(end - beg).count()/1e9;
 
 	beg = std::chrono::system_clock::now();
-#pragma omp parallel for schedule(auto)
-	for( int i = 0; i < num_map; ++i ){
-		for( int j = 0; j < U[i].n; ++j ){
-			for( int k = 0; k < my_size; ++k ) mean(i,k) += U[i](my_offset+k, j);
+#pragma omp parallel
+	{
+		for( int i = 0; i < num_map; ++i ){
+#pragma omp for schedule(auto) nowait
+			for( int j = 0; j < my_size; ++j ){
+				for( int k = 0; k < U[i].n; ++k ) mean(i,j) += U[i](my_offset+j, k);
+			}
 		}
 	}
 	mean /= U[0].n;
 	
-#pragma omp parallel for schedule(auto)
-	for( int i = 0; i < num_map; ++i ){
-		for( int j = 0; j < U[i].n; ++j ){
-			for( int k = 0; k < my_size; ++k ){
-				double v = (U[i](my_offset+k, j) - mean(i, k));
-				var(i,k) += v*v;
+#pragma omp parallel
+	{
+		for( int i = 0; i < num_map; ++i ){
+#pragma omp for schedule(auto) nowait
+			for( int j = 0; j < my_size; ++j ){
+				for( int k = 0; k < U[i].n; ++k ){
+					double v = (U[i](my_offset+j, k) - mean(i, j));
+					var(i,j) += v*v;
+				}
 			}
 		}
 	}
 	var /= U[0].n;
-	
-#pragma omp parallel for schedule(auto)
-	for( int i = 0; i < num_map; ++i ){
-		for( int j = 0; j < U[0].n; ++j ){
-			for( int k = 0; k < my_size; ++k ){
-				tmp_ret[i](k, j) = W[0][i](0,0)*(U[i](my_offset+k,j) - mean(i,k))/std::sqrt(var(i,k)+EPS) + W[0][i](0,1);
+
+#pragma omp parallel
+	{
+		for( int i = 0; i < num_map; ++i ){
+#pragma omp for schedule(auto) nowait
+			for( int j = 0; j < my_size; ++j ){
+				for( int k = 0; k < U[0].n; ++k ){
+#ifdef USE_MPI
+					tmp_ret[i](j, k) = W[0][i](0,0)*(U[i](my_offset+j,k) - mean(i,j))/std::sqrt(var(i,j)+EPS) + W[0][i](0,1);
+#else
+					ret[i](j, k) = W[0][i](0,0)*(U[i](my_offset+j,k) - mean(i,j))/std::sqrt(var(i,j)+EPS) + W[0][i](0,1);
+#endif					
+				}
 			}
 		}
 	}
@@ -272,7 +296,6 @@ std::vector<BatchNormalize::Mat> BatchNormalize::apply ( const std::vector<Mat>&
 
 #ifdef USE_MPI
 	std::vector<MPI_Request> req(num_map);
-#endif
 	if( use_func ){
 		for( int i = 0; i < num_map; ++i ){
 			beg = std::chrono::system_clock::now();
@@ -281,10 +304,8 @@ std::vector<BatchNormalize::Mat> BatchNormalize::apply ( const std::vector<Mat>&
 			t_apply_gemm += std::chrono::duration_cast<std::chrono::nanoseconds>(end - beg).count()/1e9;
 
 			beg = std::chrono::system_clock::now();
-#ifdef USE_MPI
 			MPI_Iallgatherv(&tmp_ret[i](0,0), size[rank], MPI_DOUBLE_PRECISION,
 							&ret[i](0,0), &size[0], &offset[0], MPI_DOUBLE_PRECISION, inner_world, &req[i]);
-#endif
 			end = std::chrono::system_clock::now();
 			t_apply_comm += std::chrono::duration_cast<std::chrono::nanoseconds>(end - beg).count()/1e9;
 		}
@@ -292,14 +313,22 @@ std::vector<BatchNormalize::Mat> BatchNormalize::apply ( const std::vector<Mat>&
 	else{
 		for( int i = 0; i < num_map; ++i ){
 			beg = std::chrono::system_clock::now();
-#ifdef USE_MPI
 			MPI_Iallgatherv(&tmp_ret[i](0,0), size[rank], MPI_DOUBLE_PRECISION,
 							&ret[i](0,0), &size[0], &offset[0], MPI_DOUBLE_PRECISION, inner_world, &req[i]);
-#endif
 			end = std::chrono::system_clock::now();
 			t_apply_comm += std::chrono::duration_cast<std::chrono::nanoseconds>(end - beg).count()/1e9;
 		}
 	}
+#else
+	if( use_func ){
+		for( int i = 0; i < num_map; ++i ){
+			beg = std::chrono::system_clock::now();
+			ret[i] = (*func)(ret[i], false);
+			end = std::chrono::system_clock::now();
+			t_apply_gemm += std::chrono::duration_cast<std::chrono::nanoseconds>(end - beg).count()/1e9;
+		}
+	}	
+#endif
 
 	beg = std::chrono::system_clock::now();
 #ifdef USE_MPI
